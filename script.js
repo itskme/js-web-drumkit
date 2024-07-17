37fb4922b38169cd9df5a2eebb21e7a1cbab2c56
itskme <160423464+itskme@users.noreply.github.com>
2024-07-17 21:32:14 +0000

    Add sound files
    - discovered sounds come in .wav files, changed the JS
    - input the wav files.

--- script.js
+++ script.js
@@ -13,39 +13,39 @@ document.addEventListener('keydown', event => {
 function playSound(keyCode) {
   switch (keyCode) {
     case 65:
-      var audio = new Audio('sounds/A.mp3');
+      var audio = new Audio('sounds/A.wav');
       audio.play();
       break;
     case 83:
-      var audio = new Audio('sounds/S.mp3');
+      var audio = new Audio('sounds/S.wav');
       audio.play();
       break;
     case 68:
-      var audio = new Audio('sounds/D.mp3');
+      var audio = new Audio('sounds/D.wav');
       audio.play();
       break;
     case 70:
-      var audio = new Audio('sounds/F.mp3');
+      var audio = new Audio('sounds/F.wav');
       audio.play();
       break;
     case 71:
-      var audio = new Audio('sounds/G.mp3');
+      var audio = new Audio('sounds/G.wav');
       audio.play();
       break;
     case 72:
-      var audio = new Audio('sounds/H.mp3');
+      var audio = new Audio('sounds/H.wav');
       audio.play();
       break;
     case 74:
-      var audio = new Audio('sounds/J.mp3');
+      var audio = new Audio('sounds/J.wav');
       audio.play();
       break;
     case 75:
-      var audio = new Audio('sounds/K.mp3');
+      var audio = new Audio('sounds/K.wav');
       audio.play();
       break;
     case 76:
-      var audio = new Audio('sounds/L.mp3');
+      var audio = new Audio('sounds/L.wav');
       audio.play();
       break;
     default:
